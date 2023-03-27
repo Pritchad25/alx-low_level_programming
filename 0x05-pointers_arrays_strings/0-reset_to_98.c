af8a4cbddaad638e29e2bd4aa91185f0c1286ff6
Pritchad25 <pricts29@gmail.com>
2023-03-27 01:48:25 -0700

    Task 0: A function that takes a pointer to an int as paraameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Resets the value of a variable to 98
+ * @n: variable whose value is to be reset
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
