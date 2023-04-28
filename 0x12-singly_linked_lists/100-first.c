bf8a02b7e37fb53ce2f0ae57161aac304b138245
Pritchad25 <pricts29@gmail.com>
2023-04-28 11:33:10 -0700

    A function that prints a text before the main function is executed.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,12 +2,12 @@
 
 void first(void) __attribute__ ((constructor));
 /**
- * first - the function prints a sentence before the main function
+ * bmain - the function prints a sentence before the main function
  * is executed.
  *
  * Return: nothing
  */
-void first(void)
+void __attribute__ ((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow, \n");
 	printf("I bore my house upon my back!\n");
