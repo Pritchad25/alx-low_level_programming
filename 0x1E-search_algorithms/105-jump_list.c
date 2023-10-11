c2aef2d3b205ea08336c73376b2e477160e2afca
Pritchad25 <pricts29@gmail.com>
2023-10-11 05:59:10 -0700

    A function that searches for a value in a sorted list of integers using the Jump search algorithm.

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,51 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - this function searches for a value in a sorted singly linked
+ * list of integers using jump search.
+ * @list: the pointer to the head of the linked list to search.
+ * @size: the number of nodes in the list.
+ * @value: the value to search for.
+ *
+ * Return: NULL, if the value is not present or the head of the list is NULL.
+ * Otherwise, a pointer to the first node where the value is located.
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t step, step_size;
+	listint_t *node, *jump;
+
+	if (!list || size == 0)
+	{
+		return (NULL);
+	}
+	step = 0;
+
+	/* jump step size determination */
+	step_size = sqrt(size);
+
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
+	{
+		/* Storing the previous jump node. */
+		node = jump;
+
+		for (step += step_size; jump->index < step; jump = jump->next)
+		{
+			/* if the end of the list has been reached */
+			if (jump->index + 1 == size)
+				break;
+		}
+
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
+}
